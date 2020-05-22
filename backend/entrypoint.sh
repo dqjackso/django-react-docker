8a9e17eec9773a48c04578c9a9d3e790430da7f2
dqjackso <31450465+dqjackso@users.noreply.github.com>
2020-05-22 13:54:28 -0700

    automatically create super user

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
 python manage.py makemigrations --noinput
 python manage.py migrate
 exec "$@"
